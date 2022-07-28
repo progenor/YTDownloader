0ef41f343f1de82d0d4315e41be52aa5b598aa40
progenor <nero6126@gmail.com>
2022-07-28 21:49:28 +0300

    progress bar

--- README.md
+++ README.md
@@ -12,6 +12,14 @@ Simple yt downloader
 8. wait till it finishes downloading
 9. open the directory you saved the video to with the open file button
 
+# Privacy
+
+- The program only downloads the video and saves it to the directory you chose.
+- By default it creates a new directory in ""C:\Users\[username]\Music"" called ""MUSIC_DOWNLOADS"" where the video/song is saved.
+- The program does not save any information about the video/song!
+- The program does not save any information about your computer!
+- The program does not send or use any information about your computer!
+
 # TODO
 
 - async download
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 import os
 import asyncio
 from tkinter import filedialog
+from tkinter.ttk import Progressbar
 from pytube import YouTube, Playlist
 
 
@@ -11,33 +12,59 @@ from pytube import YouTube, Playlist
 
 Dmp3 = False
 isPlaylist = False
+dest = ""
+tasks = 0
+completed_tasks = 0
 
-dest = "D:\\Music2\\MUSIC_DOWNLOADS"
+
+def get_default_path():
+    global dest
+
+    username = os.environ.get("USERNAME")
+    dest = "C:\\"+username+"\\Music\\MUSIC_DOWNLOADS"
+
+
+def update_bar(i):
+    global tasks, text_tasks, completed_tasks
+
+    bar['value'] += (i/tasks)*100
+    completed_tasks += 1
+
+    text_tasks.set(str(completed_tasks) + "/"+str(tasks))
+    window.update_idletasks()
 
 
 def status_change(stuff, tag=None):
     global status
     status.config(state=NORMAL)
-
-    status.insert(END, stuff + "\n", tag, "\n")
-
+    status.insert(END, stuff + "\n", tag)
     status.config(state=DISABLED)
+
+    window.update_idletasks()
 
 
 def browse_file():
-    global dest, label_b
+    global dest, label_dest
     dest = filedialog.askdirectory()
-    label_b.config(text="Current dest folder: " + dest)
+    label_dest.config(text="Current dest folder: " + dest)
 
     status_change("Set destination folder to " + dest)
 
 
 def get_urls():
+    global tasks, text_tasks, completed_tasks
 
     urls = []
+    bar["value"] = 0
+    text_tasks.set("0/0")
+    completed_tasks = 0
+
     input_urls = textbox.get("1.0", "end-1c")
     urls = input_urls.split("\n")
     status_change("Got urls")
+
+    tasks = len(urls)
+    text_tasks.set("0/"+str(tasks))
 
     download(urls)
 
@@ -48,10 +75,10 @@ def dowmload_videos(urls, dest):
         try:
             yt = YouTube(video)
         except:
-            status_change("Error: " + video + " is not a valid url", 'error')
+            status_change("Error: " + video + "is not a valid url", 'error')
             return
 
-        status_change("Downloading " + video.title, 'special')
+        status_change("Downloading " + yt.title, 'special')
 
         if Dmp3:
             stream = yt.streams.filter(only_audio=True).first()
@@ -66,15 +93,21 @@ def dowmload_videos(urls, dest):
             new_file = base + ".mp3"
         else:
             new_file = base + ".mp4"
-        os.rename(out_file, new_file)
+        try:
+            os.rename(out_file, new_file)
 
-        status_change("Downloaded " + video.title)
-
-    urls = []
-    status_change("Download complete!!")
+        except:
+            status_change("Error: " + yt.title +
+                          " already exists", 'error')
+            update_bar(1)
+            continue
+        status_change("Downloaded " + yt.title)
+        update_bar(1)
 
 
 def dowmload_playlists(urls, dest):
+    global tasks
+
     for playlist in urls:
         status_change("Downloading playlist " + playlist)
 
@@ -84,6 +117,7 @@ def dowmload_playlists(urls, dest):
             status_change("Error: " + p.title +
                           " is not a valid playlist url", 'error')
             return
+        tasks += len(p.videos)
 
         try:
             for video in p.videos:
@@ -101,17 +135,18 @@ def dowmload_playlists(urls, dest):
                     new_file = base + ".mp4"
                 os.rename(out_file, new_file)
                 status_change("Downloaded " + video.title)
+                update_bar(1)
         except:
             status_change("Error: " + p.title +
                           " is not a valid playlist url", 'error')
             return
         status_change("Download completed playlist " +
                       p.title, 'success')
-    status_change("Download complete!!", 'success')
+        update_bar(1)
 
 
 def download(urls):
-    global Dmp3, dest
+    global Dmp3, dest, tasks
 
     status_change("Downloading...")
 
@@ -123,12 +158,13 @@ def download(urls):
     else:
         dowmload_videos(urls, dest)
 
+    status_change("Download complete!!", 'success')
+
 
 def check_mp():
     global Dmp3
     if(x.get()):
         Dmp3 = True
-        print(Dmp3)
         status_change("File type is set to mp3")
 
     else:
@@ -153,7 +189,17 @@ def open_file_directory():
         os.startfile(dest)
     except Exception:
         status_change("Error: Folder does not exist", 'error')
+        status_change("Creating the folder!!", 'special')
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+
     status_change("Opened destination folder", 'success')
+
+
+##################################
+#  runing the main program parts
+get_default_path()
+##############################
 
 
 # create the GUI
@@ -197,14 +243,24 @@ status.place(x=430, y=130)
 
 # create the browse button
 
-label_b = Label(window, text="Current dest folder: " + dest,
-                bg='darkgrey', fg='white', font=('Arial', 10, 'bold'))
-label_b.place(x=10, y=340)
+label_dest = Label(window, text="Current dest folder: " + dest,
+                   bg='darkgrey', fg='white', font=('Arial', 10, 'bold'))
+label_dest.place(x=10, y=340)
+
 
 browse_btn = Button(window, text="Browse", bg='darkgrey',
                     fg='white', font=('Arial', 14, 'bold'), command=browse_file)
 browse_btn.place(x=10, y=380)
 
+# create the progress bar
+bar = Progressbar(window, orient="horizontal", length=300)
+bar.place(x=490, y=340)
+
+
+text_tasks = StringVar()
+text_tasks.set("0/0")
+label_task = Label(window, textvariable=text_tasks, bg='darkgrey', fg='black')
+label_task.place(x=760, y=362)
 
 # create the button
 
